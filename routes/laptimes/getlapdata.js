ff9d635285eb677da005e56d3a7855cf026058a3
Ethan Weeks <ethanweeks@ymail.com>
2016-08-12 18:11:53 -0600

    udpate db uri

--- routes/laptimes/getlapdata.js
+++ routes/laptimes/getlapdata.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var mongojs = require('mongojs'),
-	db = mongojs('mongodb://'+DBUSER+':'+DBPASSWORD+'@ds153815.mlab.com:53815/motocross', ['documents']);
+	db = mongojs('motocross', ['documents']);
 
 function getLapData(req, res) {
 	db.documents.find({ track: req.params.track.toUpperCase()}, function(err, docs) {
