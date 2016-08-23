0eb97d17783010e10bbf8ff13b5765f76547a8a4
Ethan Weeks <ethanweeks@ymail.com>
2016-08-22 22:21:27 -0600

    updated static route

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,15 @@
 var express = require('express');
 var router = express.Router();
 
+require('dotenv');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index');
+});
+
+router.get('/'+process.env.VERIFY, function(req, res) {
+	res.render(process.env.VERIFY);
 });
 
 module.exports = router;
